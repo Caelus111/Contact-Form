ce96dfa611306007f05de6cac5cdce8f86bc7187
caelus111 <wmaarouf660@gmail.com>
2024-06-17 17:06:15 +0100

    added success screen.

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
 const form = document.querySelector("form");
-const firstNameInput = form.querySelector("#firstName");
-const lastNameInput = form.querySelector("#lastName");
-
 const TextInputs = form.querySelectorAll('input[type="text"]');
+
+const inputs = form.querySelectorAll("input");
 
 const EmailInput = form.querySelector("#email");
 const MessageInput = form.querySelector("textarea");
 const radioInput = form.querySelector("#radio");
 const radioInput2 = form.querySelector("#radio2");
 const checkInput = form.querySelector("#checkbox-input");
+const dialog = document.querySelector("dialog");
 
 const validateInputs = (e) => {
   TextInputs.forEach((input) => {
@@ -46,6 +46,11 @@ const validateInputs = (e) => {
   } else {
     setSuccess(checkInput.parentElement);
   }
+
+  if (form.checkValidity()) {
+    showSuccess()
+  }
+  
 };
 
 function setError(ele) {
@@ -61,6 +66,11 @@ function setSuccess(ele) {
   const errorText = eleParent.querySelector(".error-message");
   errorText.classList.add("hide");
   ele.classList.remove("error");
+}
+
+function showSuccess() {
+  const success = document.querySelector(".success");
+  success.style.display = "flex";
 }
 
 form.addEventListener("submit", (e) => {
